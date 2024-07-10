238c61e6b8332dd44e9f1dd81ae28067d11b841c
Stephanie <145722827+Stephaniechatter@users.noreply.github.com>
2024-07-09 22:47:55 -0500

    updated Photos API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,38 +1,71 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
-import './Dictionary.css';
+import Photos from "./Photos";
+import "./Dictionary.css";
 
-export default function Dictionary() {
-  const [keyword, setKeyword] = useState("");
-  const [results, setResults] = useState(null);
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
+  let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  const handleResponse = (response) => {
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
-  };
+  }
 
-  const search = (event) => {
-    event.preventDefault();
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
 
+  function search() {
+    // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
-  };
+    axios.get(apiUrl).then(handleDictionResponse);
 
-  const handleKeywordChange = (event) => {
+    let pexelsApiKey =
+      "5Z41fUejjtTuNEsllPKjNTwjCW2wtTkZTY5wbleD9ECWjIGRfZzD3lak";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleKeywordChange(event) {
     setKeyword(event.target.value);
-  };
+  }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          onChange={handleKeywordChange}
-          placeholder="Enter a word..."
-          value={keyword}
-        />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h1>What word do you want to look up?</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          <div className="hint">
+            suggested words: sunset, wine, yoga, plant...
+          </div>
+        </section>
+        <Results results={results} />
+        <Photos photos={photos} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
--- src/Photos.js
+++ src/Photos.js
@@ -1,33 +1,28 @@
-
 import React from "react";
 import "./Photos.css";
 
-export default function Photos(props) {
-  if (props.photos.length) {
+const Photos = (props) => {
+  if (props.photos) {
     return (
       <section className="Photos">
         <div className="row">
-          {props.photos.map(function (photo, index) {
-            return (
-              <div className="col-4" key={index}>
-                <a
-                  href={photo.src.original}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src={photo.src.landscape}
-                    className="img-fluid"
-                    alt={photo.photographer}
-                  />
-                </a>
-              </div>
-            );
-          })}
+          {props.photos.map((photo, index) => (
+            <div className="col-4" key={index}>
+              <a href={photo.src.original} target="_blank" rel="noreferrer">
+                <img
+                  src={photo.src.landscape}
+                  className="img-fluid"
+                  alt={photo.photographer}
+                />
+              </a>
+            </div>
+          ))}
         </div>
       </section>
     );
   } else {
     return null;
   }
-}
+};
+
+export default Photos;
